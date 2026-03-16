4686177ee5a9f7a54f8351f1df602ba15d00ea67
Milan Patel <131247110+MilanPatel2003@users.noreply.github.com>
2026-03-16 12:24:20 +0530

    Update lookups.sql

--- schemas/lookups.sql
+++ schemas/lookups.sql
@@ -6,6 +6,36 @@ INSERT INTO genders (gender_name) VALUES
 ('Female'),
 ('Other');
 
+INSERT INTO states (state_name) VALUES
+('Andhra Pradesh'),
+('Arunachal Pradesh'),
+('Assam'),
+('Bihar'),
+('Chhattisgarh'),
+('Goa'),
+('Gujarat'),
+('Haryana'),
+('Himachal Pradesh'),
+('Jharkhand'),
+('Karnataka'),
+('Kerala'),
+('Madhya Pradesh'),
+('Maharashtra'),
+('Manipur'),
+('Meghalaya'),
+('Mizoram'),
+('Nagaland'),
+('Odisha'),
+('Punjab'),
+('Rajasthan'),
+('Sikkim'),
+('Tamil Nadu'),
+('Telangana'),
+('Tripura'),
+('Uttar Pradesh'),
+('Uttarakhand'),
+('West Bengal');
+
 
 -- =========================
 -- MARITAL STATUSES
